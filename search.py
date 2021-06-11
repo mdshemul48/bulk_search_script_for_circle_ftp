e85eca422923bb2684d1efb5cdf74d9574acc971
MD Shimul <mdshemul480@gmail.com>
2021-06-11 21:47:10 +0600

    adding git to the project

---
+++ search.py
@@ -0,0 +1,145 @@
+import sys, os
+import time
+from search_engine import search_movie
+from guessit import guessit
+
+
+def read_command_args():
+    # this will read command that givin by user :- get text path from user.
+    all_args = sys.argv
+
+    # trying to get the file name or path if not given then raise error.
+    # later i will handle the error in main exception.
+    try:
+        if len(all_args[1].strip()) == 0:
+            raise FileNotFoundError("no file name given!")
+    except:
+        raise FileNotFoundError("no file name given!")
+    return all_args[1]
+
+
+def red_text_file(text_file_path: str):
+
+    # this will read text file. make a list of all lines then return.
+    text_file = open(os.path.join(os.getcwd(), text_file_path), "r")
+    full_text_file = text_file.read()
+    full_text_line_list = full_text_file.split("\n")
+    return full_text_line_list
+
+
+class movie_title_data:
+    # this object will get movie title and return movie name and year
+    def __init__(self, movie_name) -> None:
+        self.movie_name = movie_name
+        self.movie_data_obj = guessit(movie_name)
+
+    def get_title(self):
+        return self.movie_data_obj["title"]
+
+    def get_year(self):
+        return self.movie_data_obj["year"]
+
+
+class log_the_search_result:
+    def __init__(self) -> None:
+        # creating output file
+        self.output = open("search_output.txt", "w")
+        self.output.write("script for searching movie in circleftp.net\n")
+        self.output.write("Created By MD. Shimul. ;)\n")
+        self.output.write("---------------------------------\n")
+
+    def log_movie_result(self, **kwargs):
+        # extracting data from given obj from **kwargs
+        movie_title = kwargs["movie_title"]
+        found = kwargs["found"]
+        result = kwargs["result"]
+
+        # writing movie name.
+        self.output.write("\n")
+        self.output.write("---------------------\n")
+        self.output.write(f"Movie: {movie_title} {found}.\n")
+        self.output.write(f"+---------\n")
+        self.output.write(f"|\n")
+
+        # writing movie search result
+        for movie in result:
+            title = movie["title"]
+            link = movie["link"]
+            self.output.write(f"+---{title}\n")
+            self.output.write(f"|   \---{link}\n|\n")
+        self.output.write("|\n")
+
+    def save_file(self):
+        # this will save the output file.
+        self.output.close()
+
+
+def searcher(text_line: str, log):
+
+    # checking if line empty or not. if empty then don't process.
+    if len(text_line.strip()) == 0:
+        return
+
+    # getting movie title and year..
+    try:
+        movie_title_info = movie_title_data(text_line)
+        movie_title = movie_title_info.get_title()
+        movie_year = movie_title_info.get_year()
+    except KeyError:
+        print(
+            f"{text_line} -----> didn't contain movie title or year.. maybe not both. :')"
+        )
+        return
+
+    print(f"searching: {text_line}..")
+
+    # searching the movie and getting the result..
+    searching = search_movie(movie_title, movie_year)
+    is_found = searching["found"]
+    result = searching["search_result"]
+
+    if is_found:
+        found = "already exist in the server."
+    else:
+        found = "not Found in the server."
+    # this will print movie found or not.
+    print(found)
+
+    # this will log the movie in log text file.
+    log.log_movie_result(movie_title=text_line, found=found, result=result)
+
+    # task done.
+    print("boo")
+
+
+def main():
+    # this log will create another text file and log every result.
+    log = log_the_search_result()
+    # getting user input (text file path)
+    file_name = read_command_args()
+
+    # getting all the line of the text as a list.
+    text_all_line_list = red_text_file(file_name)
+
+    # this will process 1 by 1 movie to the searcher.
+    # then searcher will extract and search the movie in our site.
+    for line in text_all_line_list:
+        try:
+            searcher(line, log)
+        except:
+            print(
+                f"{line} -----> didn't contain movie title or year.. maybe not both. :')"
+            )
+
+    # this will save the search_result log file.
+    log.save_file()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        print(err)
+
+    print("DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    time.sleep(10)
---
+++ search_engine.py
@@ -0,0 +1,65 @@
+import requests, re
+from bs4 import BeautifulSoup
+from difflib import SequenceMatcher, get_close_matches
+from guessit import guessit
+
+
+def search_movie(movie_name: str, movie_year: str):
+    # this will request a page with movie title and scrap that page.
+    search = requests.get(f"http://circleftp.net", params={"s": movie_name})
+
+    # if page status code not == 200 raise error.
+    if search.status_code != 200:
+        raise Exception("request failed. status code not 200.")
+
+    content = search.content
+    html_soup = BeautifulSoup(content, "html.parser")
+    all_article = html_soup.find_all("article")
+
+    # all movie will be added to search result obj.
+    search_result = {}
+    # found will be True when the movie name and year will be match to the searching movie.
+    found = False
+
+    for article in all_article:
+        # getting movie name and link from html document.
+        found_name = article.find("h3", class_="entry-title").text.strip()
+        link = article.find("h3", class_="entry-title").find("a").get("href")
+
+        # extacting movie name and year from full title.
+        try:
+            title = guessit(found_name)
+        except:
+            continue
+        try:
+            title_name = title["title"]
+            try:
+                title_year = title["year"]
+            except:
+                title_year = re.search(r"\d\d\d\d", found_name)[0]
+
+            # this will return a matched % between 2 strings. in my case 2 movie name.
+            match_ratio = SequenceMatcher(None, title_name, movie_name).ratio() * 100
+
+            # if both movie title ratio match more then 70% and also year are same..
+            # thats means movie already avaliable in server. found = true else movie not available in server.
+            if match_ratio >= 70:
+                if title_year == movie_year:
+                    found = True
+            search_result[f"{title_name.lower()} {title_year}"] = {
+                "title": found_name,
+                "link": link,
+            }
+        except:
+            pass
+
+    searched_content = f"{movie_name.lower()} {movie_year}"
+
+    # this will show the those movies that title and year together matched more then 25%.
+    results = get_close_matches(searched_content, search_result, n=5, cutoff=0.25)
+    data = []
+    for item in results:
+        final_result = search_result[item]
+        data.append(final_result)
+
+    return {"found": found, "search_result": data}
